9c6c0c06b9b900f5abcc5eb713cd554b85ff6bf2
Ray <ray00178@gmail.com>
2022-10-06 21:47:34 +0800

    - add(Project): 新增 ScrollView Demo

--- SwiftUIViews/ScrollViewZ.swift
+++ SwiftUIViews/ScrollViewZ.swift
@@ -16,19 +16,24 @@ struct ScrollViewZ: View {
     NavigationView {
       ScrollView {
         ForEach($names, id: \.self) { name in
-          HStack(spacing: 12.0) {
-            Image(systemName: "person")
-              .padding(.horizontal, 8.0)
-            Text("\(name.wrappedValue)")
-            Spacer()
-            Image(systemName: "arrow.right")
-              .padding(EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 12.0))
+          NavigationLink {
+            HorizontalView(title: name.wrappedValue)
+          } label: {
+            HStack(spacing: 12.0) {
+              Image(systemName: "person")
+                .padding(.horizontal, 8.0)
+              Text("\(name.wrappedValue)")
+              Spacer()
+              Image(systemName: "arrow.right")
+                .padding(EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 12.0))
+            }
+            .font(.title)
+            .tint(Color.black)
+            .frame(height: 66.0)
+            .background(Color.white)
+            .cornerRadius(8.0)
+            .padding(8.0)
           }
-          .font(.title)
-          .frame(height: 66.0)
-          .background(Color.white)
-          .cornerRadius(8.0)
-          .padding(8.0)
         }
       }
       .frame(maxWidth: .infinity)
@@ -46,5 +51,49 @@ struct ScrollViewZ: View {
 struct ScrollViewZ_Previews: PreviewProvider {
   static var previews: some View {
     ScrollViewZ()
+  }
+}
+
+// MARK: - HorizontalView
+
+struct HorizontalView: View {
+  
+  @Environment(\.dismiss) var dismiss
+  
+  @State private var colors: [Color] = [.red, .orange, .yellow, .green, .blue,
+                                        .cyan, .purple]
+
+  @State var title: String
+  
+  var body: some View {
+    NavigationView {
+      VStack(spacing: 0.0) {
+        ScrollView(.horizontal, showsIndicators: true) {
+          HStack(spacing: 16.0) {
+            ForEach(colors, id: \.self) { color in
+              Rectangle()
+                .fill(color)
+                .cornerRadius(8.0)
+                .frame(width: 200.0, height: 150.0)
+            }
+          }
+        }
+        .padding()
+        Spacer()
+      }
+    }
+    .navigationTitle(title)
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarBackButtonHidden(true)
+    .toolbar {
+      ToolbarItem(placement: .navigationBarLeading) {
+        Button {
+          dismiss()
+        } label: {
+          Image(systemName: "arrow.backward")
+            .tint(.orange)
+        }
+      }
+    }
   }
 }
